553cd2228628ff4572bab0ac13bf383902c07de0
prateekjain-test <rajsinha98761@gmail.com>
2020-06-03 20:49:33 +0530

    Update after_install.sh

--- codedeploy/demo-test/after_install.sh
+++ codedeploy/demo-test/after_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 parentdir="$(dirname "$DIR")"
-sudo chown -R ubuntu:ubuntu demo-test
+mkdir -p /home/ubuntu/testcodedeploy/demo-test
+sudo chown -R ubuntu:ubuntu testcodedeploy
 demopid=$(pgrep -f demo-test-0.0.1-SNAPSHOT.jar)
 kill -9 $demopid
-rm -rf /home/ubuntu/demo-test/revision.txt
+rm -rf /home/ubuntu/testcodedeploy/demo-test/revision.txt
 cp $parentdir/demo-test-0.0.1-SNAPSHOT.jar /tmp/demo-test-0.0.1-SNAPSHOT.jar
 java -jar /tmp/demo-test-0.0.1-SNAPSHOT.jar > /tmp/logs.txt  2>&1 &
