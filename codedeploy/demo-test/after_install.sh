873d08f735c3e0cdb09e3aa0b1536c88de23fc08
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 07:29:27 +0530

    Update after_install.sh

--- codedeploy/demo-test/after_install.sh
+++ codedeploy/demo-test/after_install.sh
@@ -7,5 +7,5 @@ mkdir -p /home/ubuntu/testcodedeploy/demo-test
 demopid=$(pgrep -f demo-test-0.0.19-SNAPSHOT.jar)
 kill -9 $demopid
 rm -rf /home/ubuntu/testcodedeploy/demo-test/revision.txt
-cp $parentdir/demo-test-0.0.19-SNAPSHOT.jar /tmp/demo-test-0.0.19-SNAPSHOT.jar
+cp $parentdir/target/demo-test-0.0.19-SNAPSHOT.jar /tmp/demo-test-0.0.19-SNAPSHOT.jar
 java -jar /tmp/demo-test-0.0.19-SNAPSHOT.jar > /tmp/logs.txt  2>&1 &
